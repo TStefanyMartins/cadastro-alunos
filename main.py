11300f37a7779110d1ff01633c0231d5e6ce5483
Stefany Martins <tatiane.s.silva16@aluno.senai.br>
2026-09-11 10:06:12 -0300

    feat: adiciona cadastro de alunos

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 alunos = []
+
+def cadastrar():
+    nome = input("Nome do aluno: ")
+    alunos.append(nome)
+    print("Aluno cadastrado.")
+    
 while True:
     print("\n--- CADASTRO DE ALUNOS ---")
+    print("1 - Cadastrar")
     print("0 - Sair")
+    
     opcao = input("Escolha: ")
     
     if opcao == "0":
         print("Programa encerrado.")
         break
-    else:   
+    elif opcao == "1":
+        cadastrar()
+    else:
         print("Opção inválida.")
